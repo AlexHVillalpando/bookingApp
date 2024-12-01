797f4f617717f86bf8facb2a60a5387b6948cee6
AlexVillalpando <42473462+AlexHVillalpando@users.noreply.github.com>
2024-12-01 01:39:55 -0600

    card

--- src/components/home/HotelCard.jsx
+++ src/components/home/HotelCard.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 function HotelCard({ hotel }) {
-	return <h2>{hotel.name}</h2>;
+	return (
+		<div className="bg-white rounded-lg shadow-md overflow-hidden m-5">
+			<div className="aspect-{1.8}">
+				<img src={hotel.images[0].url} alt={hotel.name} />
+			</div>
+			<div>
+				<h2>{hotel.name}</h2>
+				<div>
+					<span>rating</span>
+					<span>
+						{hotel.city.name}, {hotel.city.country}
+					</span>
+					<span>{hotel.price}</span>
+					<span>More info</span>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default HotelCard;
