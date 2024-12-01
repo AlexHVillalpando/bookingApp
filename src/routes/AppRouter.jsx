bc8ab6a499cb537e3e1178d441000438dffaaa93
AlexVillalpando <42473462+AlexHVillalpando@users.noreply.github.com>
2024-12-01 00:01:22 -0600

    routes

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,9 +1,25 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Outlet } from 'react-router';
 
 function AppRouter() {
 	return (
 		<Routes>
 			<Route path="/" element={<h2>Home</h2>} />
+			<Route path="/hotel/:id" element={<h2>Details</h2>} />
+			<Route path="/reservations" element={<h2>Reservations</h2>} />
+			<Route path="/reservations" element={<h2>Reservations</h2>} />
+			<Route path="/reservations" element={<h2>Reservations</h2>} />
+
+			<Route
+				element={
+					<div>
+						<h2>AuthLayout</h2>
+						<Outlet />
+					</div>
+				}
+			>
+				<Route path="login" element={<h2>Login</h2>} />
+				<Route path="register" element={<h2>Register</h2>} />
+			</Route>
 		</Routes>
 	);
 }
