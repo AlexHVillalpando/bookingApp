d5bf3343305ec27fad6b7b15556751ef4cdec874
AlexVillalpando <42473462+AlexHVillalpando@users.noreply.github.com>
2024-12-01 23:11:19 -0600

    favicon

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from 'react-dom/client';
-import './index.css';
 import { BrowserRouter } from 'react-router';
 import AppRouter from './routes/AppRouter.jsx';
+
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
 	<BrowserRouter>
