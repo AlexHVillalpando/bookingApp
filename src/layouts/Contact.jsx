5a271515412efad129a302c3864f55545bb17245
AlexVillalpando <42473462+AlexHVillalpando@users.noreply.github.com>
2024-12-08 23:28:36 -0600

    about-button-added

--- src/layouts/Contact.jsx
+++ src/layouts/Contact.jsx
@@ -1,12 +1,9 @@
-import React from 'react';
-
 function Contact() {
 	return (
 		<>
 			<div className="contact__btn">
 				<a
-					className=""
-					href="https://github.com/AlexHVillalpando/rickmorty"
+					href="https://github.com/AlexHVillalpando/bookingApp"
 					target="_blank"
 				>
 					<i className="bx bxl-github bx-tada-hover bx-md"></i>
